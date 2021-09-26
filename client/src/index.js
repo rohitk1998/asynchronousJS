2b20b857e0d9700805f1fcbb39a0d01f5f615ade
Yash Chandel <yash.codedrill@gmail.com>
2021-09-26 12:56:14 +0530

    setup the files and designed header

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Landingpage from './pages/landingpage'
+ReactDOM.render(
+  <React.StrictMode>
+    <Landingpage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
---
+++ client/src/pages/landingpage.js
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import Navbar from "../components/Navbar";
+function landingpage() {
+  return <div class="container-fluid p-3 bg-light">
+      <div className="row">
+          <div className="col-sm-4 d-flex justify-content-center">
+            <p>asynchronousJS</p>
+          </div>
+          <div className="col-sm-6">
+            <div className="container">
+            <div className="row">
+            <div className="col-sm-2">
+            <p>Home</p>
+          </div>
+          <div className="col-sm-2">
+            <p>Blog</p>
+          </div>
+          <div className="col-sm-2">
+            <p>Javascript</p>
+          </div>
+            </div>
+            </div>
+          </div>
+          <div className="col-sm-2">
+
+          <button type="button" class="btn btn-outline-dark" style={{width:'120px'}}>Sign In</button>
+
+          </div>
+      </div>
+  </div>;
+}
+
+export default landingpage;
